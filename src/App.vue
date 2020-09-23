4f9c8a023f8ed88f1262324c1b0bf4394efbf8ec
Your Name <you@example.com>
2020-09-23 15:40:11 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <moor-switch v-model="lightSwitch"></moor-switch>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      lightSwitch: false
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import MoorSwitch from './packages/switch/index.js';
+
+const components = [
+  MoorSwitch
+]
+
+const install = function(Vue, opts = {}) {
+  components.map(component => {
+    Vue.component(component.name, component);
+  })
+}
+
+/* 支持使用标签的方式引入 */
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue);
+}
+
+export default {
+  install,
+  MoorSwitch
+}
